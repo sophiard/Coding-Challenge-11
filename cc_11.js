296f4290908bf0ea00712fe1fb8f7ed3f2c0a28c
Sophia Dolan <25srdolan@gmail.com>
2025-03-01 17:59:22 -0500

    task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -27,4 +27,41 @@ class Book {
   console.log("Task 1 part 2:",book1.getDetails());
 
   //logged results 
-  
+
+
+
+ //"Task 2 - Created Borrower Class 
+
+  class Borrower {
+    constructor(name, borrowerID) {
+        this.name = name;
+        this.borrowerID = borrowerID;
+        this.borrowedBooks = [];
+        //created a class borrower
+    };
+    borrowBook(book) { 
+        if (!this.borrowedBooks.includes(book)) { 
+            this.borrowedBooks.push(book); 
+        } else {
+            console.log(`${book} This book has already been borrowed. `);
+        };
+    }; //created a method to borrow books 
+    // added method that shos if book has alreayd been borrowed or if not adds to array
+
+    returnBook(book) { 
+       const index = this.borrowedBooks.indexOf(book); 
+        if (index !== -1) { 
+            this.borrowedBooks.splice(index, 1) 
+        } else {
+            console.log(`${book} hasn't been borrowed`) //if book is not in array
+        }//created method that retunrs books and to find the borrowed ones in arraY
+        //Also removed book from borrow array
+    }
+}
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log("Task 2 part 1", borrower1.borrowedBooks); 
+
+borrower1.returnBook("The Great Gatsby");
+console.log("Task 2 part 2", borrower1.borrowedBooks); 
+//logged outputs for task 2 
